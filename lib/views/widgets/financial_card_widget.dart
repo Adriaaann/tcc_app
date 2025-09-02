5e836cdfe0093c20c0cb0918e68132fa547393dc
Adriaaann <adriansouza04@gmail.com>
2025-09-02 14:15:52 -0300

    refactor: FinancialCard

--- lib/views/widgets/financial_card_widget.dart
+++ lib/views/widgets/financial_card_widget.dart
@@ -10,28 +10,24 @@ class FinancialCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Column(
-    children: List.generate(items.length, (index) {
-      final item = items[index];
-      return ListTile(
-        contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-        leading: _DisplayIcon(item: item),
-        title: _TileBody(item: item),
-      );
-    }),
-  );
-}
-
-class _DisplayIcon extends StatelessWidget {
-  const _DisplayIcon({required this.item});
-
-  final FinancialItem item;
-
-  @override
-  Widget build(BuildContext context) => CircleAvatar(
-    backgroundColor: context.colorScheme.primaryContainer.withValues(
-      alpha: 0.5,
-    ),
-    child: Icon(item.icon, color: context.colorScheme.primary, size: 24),
+    children: items
+        .map(
+          (item) => ListTile(
+            contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+            leading: CircleAvatar(
+              backgroundColor: context.colorScheme.primaryContainer.withValues(
+                alpha: 0.5,
+              ),
+              child: Icon(
+                item.icon,
+                color: context.colorScheme.primary,
+                size: 24,
+              ),
+            ),
+            title: _TileBody(item: item),
+          ),
+        )
+        .toList(),
   );
 }
 
@@ -41,73 +37,48 @@ class _TileBody extends StatelessWidget {
   final FinancialItem item;
 
   @override
-  Widget build(BuildContext context) => Row(
-    crossAxisAlignment: CrossAxisAlignment.end,
-    children: [
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ConstrainedBox(
-            constraints: BoxConstraints(
-              maxWidth: MediaQuery.sizeOf(context).width * 0.4,
+  Widget build(BuildContext context) {
+    final width = MediaQuery.sizeOf(context).width * 0.4;
+
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: width),
+              child: Row(
+                spacing: 4,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    '${item.hour.hour.toString().padLeft(2, '0')}:${item.hour.minute.toString().padLeft(2, '0')}',
+                    style: context.textTheme.bodySmall?.copyWith(
+                      color: context.colorScheme.tertiary,
+                    ),
+                  ),
+                  Flexible(
+                    child: Text(
+                      item.title,
+                      style: context.textTheme.bodyLarge,
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            child: Row(
-              spacing: 4,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                _DisplayTime(item: item),
-                _DisplayTitle(item: item),
-              ],
+            Text(
+              item.value,
+              style: context.textTheme.bodyLarge?.copyWith(fontSize: 20),
+              softWrap: false,
             ),
-          ),
-          _DisplayValue(item: item),
-        ],
-      ),
-      if (item.tags.isNotEmpty) ChipList(chips: item.tags),
-    ],
-  );
-}
-
-class _DisplayValue extends StatelessWidget {
-  const _DisplayValue({required this.item});
-
-  final FinancialItem item;
-
-  @override
-  Widget build(BuildContext context) => Text(
-    item.value,
-    style: context.textTheme.bodyLarge?.copyWith(fontSize: 20),
-    softWrap: false,
-  );
-}
-
-class _DisplayTitle extends StatelessWidget {
-  const _DisplayTitle({required this.item});
-
-  final FinancialItem item;
-
-  @override
-  Widget build(BuildContext context) => Flexible(
-    child: Text(
-      item.title,
-      style: context.textTheme.bodyLarge,
-      softWrap: false,
-      overflow: TextOverflow.ellipsis,
-      maxLines: 1,
-    ),
-  );
-}
-
-class _DisplayTime extends StatelessWidget {
-  const _DisplayTime({required this.item});
-
-  final FinancialItem item;
-
-  @override
-  Widget build(BuildContext context) => Text(
-    '${item.hour.hour.toString().padLeft(2, '0')}:${item.hour.minute.toString().padLeft(2, '0')}',
-    style: context.textTheme.bodySmall?.copyWith(
-      color: context.colorScheme.tertiary,
-    ),
-  );
+          ],
+        ),
+        if (item.tags.isNotEmpty) ChipList(chips: item.tags),
+      ],
+    );
+  }
 }
